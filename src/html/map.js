3d69b9cfaff4a423de29c50812a00316b6a574a5
Evan W. Patton <ewpatton@gmail.com>
2011-12-04 21:58:48 +0000

    Added controls to map.

--- src/html/map.js
+++ src/html/map.js
@@ -50,6 +50,8 @@ function initialize() {
   if (GBrowserIsCompatible()) {
     map = new GMap2(document.getElementById("map_canvas"));
     map.setCenter(new GLatLng(37.4419, -122.1419), 10);
+    var c = new GLargeMapControl();
+    map.addControl(c, c.getDefaultPosition());
     geocoder = new GClientGeocoder();
   }
 }
