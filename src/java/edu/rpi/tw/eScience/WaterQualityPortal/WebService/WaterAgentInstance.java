098e193205249abc7e0f1ad7440a92a0ce4f309c
Ping Wang <pingwang.cs@gmail.com>
2012-04-01 19:50:43 +0000

    updates for huc

--- src/java/edu/rpi/tw/eScience/WaterQualityPortal/WebService/WaterAgentInstance.java
+++ src/java/edu/rpi/tw/eScience/WaterQualityPortal/WebService/WaterAgentInstance.java
@@ -751,7 +751,8 @@ public class WaterAgentInstance implements HttpHandler {
 					listHealthEffects(arg0, params);
 				}
 				else if(method.equalsIgnoreCase("getHUC8Codes")) {
-					System.err.println("getHUC8Codes");
+					//System.err.println("getHUC8Codes");
+					log.info("getHUC8Codes");
 					WaterEntityAgent.getHUC8Codes(arg0, params, log);
 				}
 				else if(method.equalsIgnoreCase("getHUC8CodesOneState")) {
