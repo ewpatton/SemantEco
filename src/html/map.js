d0d40f31efd5fe25d96de8ea5e429b0002dd7e09
Ping Wang <pingwang.cs@gmail.com>
2012-07-15 22:34:07 +0000

    updated

--- src/html/map.js
+++ src/html/map.js
@@ -340,8 +340,6 @@ function onchange_species_selection() {
 		}
 	}
 
-	document.getElementById("regulation").selectedIndex = 0;
-
 	var spcIndex = 'human';
 	if (species == "Aquatic-life")
 		spcIndex = 'aquatic';
