1a0e7cbcaa57dd7a1e6b89b7e59e4b092f5d0e5c
Patrice Seyed <apseyed@gmail.com>
2012-11-04 23:46:31 +0000

    updated to test facet, which tests zip code

--- facets/test-facet/src/main/java/edu/rpi/tw/escience/waterquality/test/testFacet.java
+++ facets/test-facet/src/main/java/edu/rpi/tw/escience/waterquality/test/testFacet.java
@@ -1,6 +1,8 @@
 package edu.rpi.tw.escience.waterquality.test;
 
 //import com.google.common.io.CharStreams;
+import static edu.rpi.tw.escience.waterquality.query.Query.VAR_NS;
+
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Model;
 
@@ -11,7 +13,10 @@ import edu.rpi.tw.escience.waterquality.QueryMethod;
 import edu.rpi.tw.escience.waterquality.Request;
 import edu.rpi.tw.escience.waterquality.Resource;
 import edu.rpi.tw.escience.waterquality.SemantAquaUI;
+import edu.rpi.tw.escience.waterquality.query.NamedGraphComponent;
 import edu.rpi.tw.escience.waterquality.query.Query;
+import edu.rpi.tw.escience.waterquality.query.QueryResource;
+import edu.rpi.tw.escience.waterquality.query.Variable;
 import edu.rpi.tw.escience.waterquality.query.Query.Type;
 //import org.apache.http.client.methods.*;
 import org.apache.log4j.Logger;
@@ -23,13 +28,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 
 
 
 public class testFacet implements Module{
 
-	private static final String POL_NS = "http://escience.rpi.edu/ontology/semanteco/2/0/pollution.owl#";
+	private static final String POL_NS = "http://escience.rpi.edu/ontology/semenateco/2/0/pollution.owl#";
 	private static final String WATER_NS = "http://escience.rpi.edu/ontology/semanteco/2/0/water.owl#";
 	private static final String RDFS_NS = "http://www.w3.org/2000/01/rdf-schema#";
 	private static final String RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
@@ -51,15 +58,36 @@ public class testFacet implements Module{
 	private static final String SOURCE_VAR = "source";
 	private static final String LABEL_VAR = "label";
 	private static final String VALUE = "value";
+	private static final String SITE_VAR = "site";
+	private Request request = null;
+	private String countyCode = null;
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public testFacet() {
+		// default constructor
 	}
 	
 	public testFacet(final Request request, final ModuleConfiguration config){
 		//super(request, config);
-		
+		this.log = request.getLogger();
+		this.config = config;
+		this.request = request;
+		String state = (String)request.getParam("state");
+		if(state == null || state.isEmpty()) {
+			throw new IllegalArgumentException("State parameter not supplied. Expected two digit state abbreviation, e.g. CA.");
+		}
+		try {
+			this.countyCode = (String)request.getParam("county");
+		}
+		catch(Exception e) {
+			throw new IllegalArgumentException("County parameter not supplied.", e);
+		}
+	
 	}
 	
 	
@@ -122,12 +150,46 @@ public class testFacet implements Module{
 	
 	@QueryMethod
 	public String queryBirdPopulation(Request request){
-		
+		/*
 		final String siteUri = (String)request.getParam("organism");
-		
-		final Query query = config.getQueryFactory().newQuery(Type.SELECT);
 		return null;
+		*/
+		final Query query = config.getQueryFactory().newQuery(Type.SELECT);
 		
+		//Variables
+		final Variable site = query.getVariable(VAR_NS+SITE_VAR);
+
+		
+		//URIs
+		final QueryResource dataSet = query.getResource("http://sparql.tw.rpi.edu/source/akn/dataset/GBBC_CSV/version/2012-Oct-19");
+		final QueryResource inDataSet = query.getResource("http://rdfs.org/ns/void#inDataset");
+		                                                 //http://rdfs.org/ns/void#inDataset
+		//build query
+		Set<Variable> vars = new LinkedHashSet<Variable>();
+		vars.add(site);
+		query.setVariables(vars);
+		//query pattern
+		//query.addPattern(site, inDataSet, dataSet);
+		final NamedGraphComponent graph = query.getNamedGraph("http://was.tw.rpi.edu/bird-data");
+		graph.addPattern(site, inDataSet, dataSet);
+
+		// ?m void:inDataSet <http://sparql.tw.rpi.edu/source/akn/dataset/GBBC_CSV/version/2012-Oct-19>
+		
+		return config.getQueryExecutor(request).accept("application/json").executeLocalQuery(query);		
+	}
+	
+	@QueryMethod
+	public String queryAirData(Request request){
+		final Query query = config.getQueryFactory().newQuery(Type.SELECT);
+		final Variable measurement = query.getVariable(VAR_NS+"measurement");
+		final QueryResource polHasCounty = query.getResource(POL_NS+"hasCounty");
+		final QueryResource polHasState = query.getResource(POL_NS+"hasState");
+		String county = (String) request.getParam("county");
+		//String state = (String) request.getParam("stateCode");
+		final NamedGraphComponent graph = query.getNamedGraph("http://was.tw.rpi.edu/air-measurement-data");
+		graph.addPattern(measurement, polHasCounty, county,null);
+		//graph.addPattern(measurement, polHasState, state,null);
+		return config.getQueryExecutor(request).accept("application/json").executeLocalQuery(query);		
 	}
 	
 
