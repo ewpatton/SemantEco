5fe8428788bc905eacfe0bd5c20e6a7968e7d592
Evan W. Patton <ewpatton@gmail.com>
2012-12-21 21:05:08 +0000

    Added comments for PropertyPathImpl

--- query/src/main/java/edu/rpi/tw/escience/waterquality/query/impl/PropertyPathImpl.java
+++ query/src/main/java/edu/rpi/tw/escience/waterquality/query/impl/PropertyPathImpl.java
@@ -2,10 +2,25 @@ package edu.rpi.tw.escience.waterquality.query.impl;
 
 import edu.rpi.tw.escience.waterquality.query.QueryResource;
 
+/**
+ * PropertyPathImpl provides a wrapper around a
+ * SPARQL 1.1 compliant property path string so
+ * that property paths can be used in place of
+ * Variable and URI-based query resources.
+ * @author ewpatton
+ *
+ */
 public class PropertyPathImpl implements QueryResource {
 
 	private final String path;
 	
+	/**
+	 * Constructs a PropertyPathImpl for the given
+	 * path. It DOES NOT check for syntactic or
+	 * semantic correctness. It is up to the caller
+	 * to perform such checks.
+	 * @param path
+	 */
 	public PropertyPathImpl(final String path) {
 		this.path = path;
 	}
