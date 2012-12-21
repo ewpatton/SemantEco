631fdb9c1e9f9db0d0f25716729de2df0c63717c
Evan W. Patton <ewpatton@gmail.com>
2012-12-21 21:05:29 +0000

    Added comments for TestQueryFactory

--- test/src/main/java/edu/rpi/tw/escience/waterquality/test/TestQueryFactory.java
+++ test/src/main/java/edu/rpi/tw/escience/waterquality/test/TestQueryFactory.java
@@ -3,12 +3,20 @@ package edu.rpi.tw.escience.waterquality.test;
 import edu.rpi.tw.escience.waterquality.query.Query;
 import edu.rpi.tw.escience.waterquality.query.Query.Type;
 
+/**
+ * TestQueryFactory provides a basic implementation of
+ * QueryFactory that can be used for writing unit tests.
+ * @author ewpatton
+ *
+ */
 public class TestQueryFactory extends MockQueryFactory {
 
+	@Override
 	public Query newQuery() {
 		return newQuery(Type.SELECT);
 	}
 	
+	@Override
 	public Query newQuery(Type type) {
 		return new TestQuery(type);
 	}
