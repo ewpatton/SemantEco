020b11a28071c821a35f323d102d72248dc0331a
Brendan E Ashby <ashbyb@rpi.edu>
2013-06-05 19:32:14 -0400

    pushing latest changes to my facet

--- facets/semanteco-geo-facet/src/main/java/edu/rpi/tw/escience/semanteco/geo/SemantEcoGeoModule.java
+++ facets/semanteco-geo-facet/src/main/java/edu/rpi/tw/escience/semanteco/geo/SemantEcoGeoModule.java
@@ -14,6 +14,16 @@ import edu.rpi.tw.escience.semanteco.Request;
 import edu.rpi.tw.escience.semanteco.Resource;
 import edu.rpi.tw.escience.semanteco.SemantEcoUI;
 import edu.rpi.tw.escience.semanteco.query.Query;
+import edu.rpi.tw.escience.semanteco.Domain;
+import edu.rpi.tw.escience.semanteco.ProvidesDomain;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
 
 public class SemantEcoGeoModule implements Module {
 
@@ -66,8 +76,37 @@ public class SemantEcoGeoModule implements Module {
 	
 	@QueryMethod
 	public String testMethod(final Request request) {
-		request.getLogger().debug("Hey you!");
+		request.getLogger().debug("Hey you! Test 1.");
 		return null;
 	}
-
+	
+	public List<Domain> getDomains(final Request request) {
+		List<Domain> domains = new ArrayList<Domain>();
+	    Domain semantGeoDomain = config.getDomain(URI.create("http://purl.org/twc/SemantGeo/"), true);
+	    // add data sources, regulations, and data types here
+	    domains.add(semantGeoDomain);
+	    semantGeoDomain.setLabel("SemantGeo");
+		addDataSources(semantGeoDomain, request);
+		addRegulations(semantGeoDomain);
+		addDataTypes(semantGeoDomain);
+	    return domains;
+	 }
+	
+	protected void addDataSources(final Domain domain, final Request request) {
+		// TODO query for data sources and add them here (cf {@link WaterDataProviderModule#addDataSources(Domain, Request)})
+		domain.addSource(URI.create("http://sparql.tw.rpi.edu/source/darrin-fresh-water"), "Darrin Fresh Water");
+	}
+	
+	protected void addRegulations(final Domain domain) {
+		// TODO query for regulations and add them here
+		//domain.addRegulation(URI.create("http://was.tw.rpi.edu/semanteco/regulations/EPA-air-regulation.owl"), "Darrin Fresh Water");
+	}
+	
+	protected void addDataTypes(final Domain domain) {
+		// change to icon names here should also occur in air-data-provider.js
+		Resource res = config.getResource("clean-air.png");
+		domain.addDataType("clean-air", "DFW - Temp - 1", res);
+		res = config.getResource("polluted-air.png");
+		domain.addDataType("polluted-air", "DFW - Temp - 2", res);
+	}
 }
