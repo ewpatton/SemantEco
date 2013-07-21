66a9b6331d5f8d6e63066b8f09fc4e8bdc5092ca
Katie Chastain <chastk@rpi.edu>
2013-07-21 14:17:24 -0400

    updates for reading package-level metadata

--- webapp/src/main/webapp/js/annotator/annotator-js-parser.js
+++ webapp/src/main/webapp/js/annotator/annotator-js-parser.js
@@ -33,16 +33,72 @@ function queryForEnhancing(){
 
 //	Reads in Params File writes triples related to prefixes, source, dataset, and versionId.
 //	Takes the headers and generates column specific enhancements.
-//	* prefixes in here, too? (definitely need ov, qb)
-//	Package-level data from the html:
-//		"source_info"/"source_add_new"
-//		"dataset_info"/"name_add_new"
-//		"version_info"
+
 function generateParmsFileFromHeaders(){
+	//	Prefixes
+	//	* Definitely need ov, qb
+	
+	//	Package-level data from the html:
+	//		"source_info"/"source_add_new"
+	//		"dataset_info"/"name_add_new"
+	//		"version_info"
+	var baseURI = "conversion:base_uri\t\"http://purl.org/twc/semantgeo\"^^xsd:anyURI;\n";
+	var source = "conversion:source_identifier\t";
+	var dataset = "conversion:dataset_identifier\t";
+	var version = "conversion:version_identifier\t";
+	var addSource, addDataset, addVersion;
+	// if the user specified a source, use that...
+	// * NOTE that in the future, we will probably want to 
+	// have some kind of checking here to make sure the user
+	// hasn't done anything silly (like only entered a space)
+	if ( (document.getElementById("source_add_new").value) ){
+		addSource = "\"" + (document.getElementById("source_add_new").value) + "\";\n";
+		source += addSource;
+	}
+	// ... otherwise, check the dropdown... 
+	else if( (document.getElementById("source_info").value) ) {
+		addSource = "\"" + (document.getElementById("source_info").value) + "\";\n";
+		source += addSource;
+	}
+	// ... and let the user know if everything's null
+	else 
+		alert("Data Source field left blank!");
+	
+	// Do the same thing for the dataset name
+	if ( (document.getElementById("dataset_add_new").value) ){
+		addDataset = "\"" + (document.getElementById("dataset_add_new").value) + "\";\n";
+		dataset += addDataset;
+	}
+	else if( (document.getElementById("dataset_info").value) ) {
+		addDataset = "\"" + (document.getElementById("dataset_info").value) + "\";\n";
+		dataset += addDataset;
+	}
+	else 
+		alert("Dataset Name field left blank!");
+		
+	// Version is only a text field
+	// Just make sure that's not blank
+	// * As with the above, we may want to check/clean user input a little
+	if ( (document.getElementById("version_info").value) ){
+		addVersion = "\"" + (document.getElementById("version_info").value) + "\";\n";
+		version += addVersion;
+	}
+	else 
+		alert("Version field left blank!");
+	
+	console.log(baseURI + source + dataset + version);
+	
+	
+	
+	// a loop goes here
+	// call queryForPropertyToEnhance
+	// call queryFor HeaderToEnhance
 }// /generateParmsFileFromHeaders
 
 function queryForPropertyToEnhance(){
+	
 }// /queryForPropertyToEnhance
 
 function queryForHeaderToEnhance(){
+	
 }// /queryForHeaderToEnhance
